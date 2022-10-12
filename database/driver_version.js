a36cbc0c90e5d5c876da764d27147f119467bcaf
Florian Breit <77027788+flnbrt@users.noreply.github.com>
2022-10-12 11:02:54 +0200

    added check for node.js driver version

---
+++ database/driver_version.js
@@ -0,0 +1,19 @@
+const mongodb = require('mongodb');
+const mongodbClient = mongodb.MongoClient;
+
+// set host and port for mongodb connection
+const mongodbHost = '130.61.73.74';
+const mongodbPort = '27017';
+
+// set mongodb connection url
+const url = 'mongodb://' + mongodbHost + ':' + mongodbPort;
+
+// check MongoDB connection and print node.js driver version into the console
+mongodbClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+
+    if (err) throw err;
+
+    console.log(client.topology.clientInfo);
+
+    client.close();
+});
